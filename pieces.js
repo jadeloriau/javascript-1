c8c1536ec367ed7952068b074a9d940f4e2ee44c
Jade Loriau <jade.loriau@outlook.com>
2025-10-22 12:45:07 +0200

    correction

--- pieces.js
+++ pieces.js
@@ -1,6 +1,9 @@
-const response = await fetch('pieces-auto.json');
-const pieces = await response.json();
 
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
+const pieces = await reponse.json();
+
+// Création des balises 
 const article= pieces[0];
 const imgElem = document.createElement("img");
 imgElem.src=article.image;
@@ -9,17 +12,18 @@ nomElem.innerText= article.nom;
 const catElem = document.createElement("p");
 catElem.innerText= article.categorie??"(aucune catégorie)";
 const prixElem = document.createElement("p");
-prixElem.innerText='Prix : ${article.prix} (${article.prix}<35?"€":"€€€")';
+prixElem.innerText=`Prix : ${article.prix} (${article.prix<35?"€":"€€€"})`;
 const descElem = document.createElement("p");
 descElem.innerText = article.description??"pas de description pour le moment";
 const dispElem = document.createElement("p");
 dispElem.innerText = article.disponibilite?"Rupture de stock":"En stock";
 
+//Rattachement de nos balises au DOM
 
-const sectionfiches = document.querySelector(".fiches");
-sectionfiches.appendChild(imgElem);
-sectionfiches.appendChild(nomElem)
-sectionfiches.appendChild(prixElem)
-sectionfiches.appendChild(catElem);
-sectionfiches.appendChild(descElem);
-sectionfiches.appendChild(dispElem);
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imgElem);
+sectionFiches.appendChild(nomElem)
+sectionFiches.appendChild(prixElem)
+sectionFiches.appendChild(catElem);
+sectionFiches.appendChild(descElem);
+sectionFiches.appendChild(dispElem);
