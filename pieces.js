c3d0951ce021e5595420c9fb54c902209a344532
Jade Loriau <jade.loriau@outlook.com>
2025-10-29 15:15:00 +0100

    ajout article

--- pieces.js
+++ pieces.js
@@ -3,27 +3,35 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-// Création des balises 
-const article= pieces[0];
+for(let i=0; i<pieces.length;i++){
 const imgElem = document.createElement("img");
-imgElem.src=article.image;
+imgElem.src=pieces[i].image;
 const nomElem = document.createElement("h2");
-nomElem.innerText= article.nom;
+nomElem.innerText= pieces[i].nom;
 const catElem = document.createElement("p");
-catElem.innerText= article.categorie??"(aucune catégorie)";
+catElem.innerText= pieces[i].categorie??"(aucune catégorie)";
 const prixElem = document.createElement("p");
-prixElem.innerText=`Prix : ${article.prix} (${article.prix<35?"€":"€€€"})`;
+prixElem.innerText=`Prix : ${pieces[i].prix} (${pieces[i].prix<35?"€":"€€€"})`;
 const descElem = document.createElement("p");
-descElem.innerText = article.description??"pas de description pour le moment";
+descElem.innerText = pieces[i].description??"pas de description pour le moment";
 const dispElem = document.createElement("p");
-dispElem.innerText = article.disponibilite?"Rupture de stock":"En stock";
+dispElem.innerText = pieces[i].disponibilite?"Rupture de stock":"En stock";
 
 //Rattachement de nos balises au DOM
-
 const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imgElem);
-sectionFiches.appendChild(nomElem)
-sectionFiches.appendChild(prixElem)
-sectionFiches.appendChild(catElem);
-sectionFiches.appendChild(descElem);
-sectionFiches.appendChild(dispElem);
+const pieceElement = document.createElement("article");
+sectionFiches.appendChild(pieceElement)
+
+pieceElement.appendChild(imgElem);
+pieceElement.appendChild(nomElem)
+pieceElement.appendChild(prixElem)
+pieceElement.appendChild(catElem);
+pieceElement.appendChild(descElem);
+pieceElement.appendChild(dispElem);
+
+
+}
+
+
+
+
